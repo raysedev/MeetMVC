9800914b0097aa23f19e40ecfe0f08cf27b6c67b
Rayse <raysedev@protonmail.com>
2022-04-25 05:07:34 +0300

    Add project files.

---
+++ MeetMVC/Data/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MeetMVC.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string age { get; set; }
+        public string about { get; set; }
+        public string lookingFor { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public string gender { get; set; }
+        public string sexuality { get; set; }
+    }
+}
